8b332fe8ef4c4255b800c1fe2e171af139b3dd67
alanakov <angelikovalski@gmail.com>
2025-07-03 00:57:12 -0300

    feat: add social media icons and footer component to enhance site layout

--- portfolio-frontend/src/App.tsx
+++ portfolio-frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">{/* Conteúdo principal vai aqui */}</main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
