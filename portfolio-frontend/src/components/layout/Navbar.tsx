b4eec50958558becfdd61c3fe2e024d3c0046a88
alanakov <angelikovalski@gmail.com>
2025-07-03 21:10:40 -0300

    feat: update Navbar component to improve mobile menu functionality and enhance background color styling

--- portfolio-frontend/src/components/layout/Navbar.tsx
+++ portfolio-frontend/src/components/layout/Navbar.tsx
@@ -16,10 +16,11 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 px-4 lg:px-20 bg-background">
+    <nav className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-4 px-4 lg:px-20 bg-[var(--color-background)]">
       <div className="flex items-center">
         <label className="text-2xl font-bold">Alana</label>
       </div>
+      {/* Desktop links */}
       <div className="hidden lg:flex items-center gap-8">
         <a href="#home" className="flex hover:text-white transition-colors">
           <p className="text-redPrimary">#</p>
@@ -103,98 +104,110 @@ export default function Navbar() {
         ></span>
       </button>
       {/* Mobile drawer menu */}
-      <div
-        className={`fixed top-0 right-0 h-full w-72 bg-background-secondary z-50 shadow-lg transform transition-transform duration-300 ease-in-out ${menuOpen ? "translate-x-0" : "translate-x-full"} lg:hidden flex flex-col p-8 gap-8`}
-        style={{ minWidth: "16rem" }}
-      >
-        <button
-          className="self-end mb-4 text-2xl text-gray-400 hover:text-white"
-          onClick={() => setMenuOpen(false)}
-          aria-label="Fechar menu"
-        >
-          &times;
-        </button>
-        <a
-          href="#home"
-          className="text-xl font-semibold hover:text-white transition-colors"
-          onClick={() => setMenuOpen(false)}
-        >
-          <span className="text-redPrimary">#</span>
-          {t("navbar.home")}
-        </a>
-        <a
-          href="#about-me"
-          className="text-xl font-semibold hover:text-white transition-colors"
-          onClick={() => setMenuOpen(false)}
-        >
-          <span className="text-redPrimary">#</span>
-          {t("navbar.about")}
-        </a>
-        <a
-          href="#projects"
-          className="text-xl font-semibold hover:text-white transition-colors"
-          onClick={() => setMenuOpen(false)}
-        >
-          <span className="text-redPrimary">#</span>
-          {t("navbar.projects")}
-        </a>
-        <a
-          href="#contact"
-          className="text-xl font-semibold hover:text-white transition-colors"
-          onClick={() => setMenuOpen(false)}
-        >
-          <span className="text-redPrimary">#</span>
-          {t("navbar.contact")}
-        </a>
-        <div className="flex items-center gap-4 mt-4">
-          <Select value={selectedLanguage} onValueChange={handleLanguageChange}>
-            <SelectTrigger className="border-none cursor-pointer font-semibold ">
-              <div className="flex items-center gap-2">
-                <img
-                  src={
-                    selectedLanguage === "pt"
-                      ? "/assets/images/flag-brazil.svg"
-                      : "/assets/images/flag-usa.svg"
-                  }
-                  alt={
-                    selectedLanguage === "pt" ? "Bandeira do Brasil" : "US Flag"
-                  }
-                  className="w-5 h-4 object-cover rounded-sm"
-                />
-                <span>{selectedLanguage === "pt" ? "PT-BR" : "EN-US"}</span>
-              </div>
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem
-                className="cursor-pointer hover:text-white"
-                value="pt"
+      {menuOpen && (
+        <>
+          <div
+            className="fixed inset-0 bg-transparent z-40 lg:hidden"
+            onClick={() => setMenuOpen(false)}
+          ></div>
+          <div
+            className={`fixed top-0 right-0 h-full w-48 bg-[var(--color-backgroundSecondary)] z-50 shadow-lg transform transition-transform duration-300 ease-in-out ${menuOpen ? "translate-x-0" : "translate-x-full"} lg:hidden flex flex-col p-8 gap-8`}
+          >
+            <button
+              className="self-end mb-4 text-2xl text-gray-400 hover:text-white"
+              onClick={() => setMenuOpen(false)}
+              aria-label="Fechar menu"
+            >
+              &times;
+            </button>
+            <a
+              href="#home"
+              className="text-xl font-semibold hover:text-white transition-colors"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span className="text-redPrimary">#</span>
+              {t("navbar.home")}
+            </a>
+            <a
+              href="#about-me"
+              className="text-xl font-semibold hover:text-white transition-colors"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span className="text-redPrimary">#</span>
+              {t("navbar.about")}
+            </a>
+            <a
+              href="#projects"
+              className="text-xl font-semibold hover:text-white transition-colors"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span className="text-redPrimary">#</span>
+              {t("navbar.projects")}
+            </a>
+            <a
+              href="#contact"
+              className="text-xl font-semibold hover:text-white transition-colors"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span className="text-redPrimary">#</span>
+              {t("navbar.contact")}
+            </a>
+            <div className="flex items-center gap-4 mt-4">
+              <Select
+                value={selectedLanguage}
+                onValueChange={handleLanguageChange}
               >
-                <div className="flex items-center gap-2">
-                  <img
-                    src="/assets/images/flag-brazil.svg"
-                    alt="Bandeira do Brasil"
-                    className="w-5 h-4 object-cover rounded-sm"
-                  />
-                  PT-BR
-                </div>
-              </SelectItem>
-              <SelectItem
-                className="cursor-pointer hover:text-white"
-                value="en"
-              >
-                <div className="flex items-center gap-2">
-                  <img
-                    src="/assets/images/flag-usa.svg"
-                    alt="US Flag"
-                    className="w-5 h-4 object-cover rounded-sm"
-                  />
-                  EN-US
-                </div>
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+                <SelectTrigger className="border-none cursor-pointer font-semibold ">
+                  <div className="flex items-center gap-2">
+                    <img
+                      src={
+                        selectedLanguage === "pt"
+                          ? "/assets/images/flag-brazil.svg"
+                          : "/assets/images/flag-usa.svg"
+                      }
+                      alt={
+                        selectedLanguage === "pt"
+                          ? "Bandeira do Brasil"
+                          : "US Flag"
+                      }
+                      className="w-5 h-4 object-cover rounded-sm"
+                    />
+                    <span>{selectedLanguage === "pt" ? "PT-BR" : "EN-US"}</span>
+                  </div>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem
+                    className="cursor-pointer hover:text-white"
+                    value="pt"
+                  >
+                    <div className="flex items-center gap-2">
+                      <img
+                        src="/assets/images/flag-brazil.svg"
+                        alt="Bandeira do Brasil"
+                        className="w-5 h-4 object-cover rounded-sm"
+                      />
+                      PT-BR
+                    </div>
+                  </SelectItem>
+                  <SelectItem
+                    className="cursor-pointer hover:text-white"
+                    value="en"
+                  >
+                    <div className="flex items-center gap-2">
+                      <img
+                        src="/assets/images/flag-usa.svg"
+                        alt="US Flag"
+                        className="w-5 h-4 object-cover rounded-sm"
+                      />
+                      EN-US
+                    </div>
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </>
+      )}
     </nav>
   );
 }
